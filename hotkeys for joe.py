17414343899c3cddc850da1e68075cb383fc14f3
Cherveny <RCherveny@jewels.com>
2022-07-19 08:21:15 -0400

    more organized, and added workday and google commands.

--- hotkeys for joe.py
+++ hotkeys for joe.py
@@ -16,40 +16,44 @@ from pyautogui import doubleClick
 
 import sys
 
+import webbrowser
+
 is_alive = True
 
-dont_fuck_up = False
+dfu = False
 
 usual_sleep_time = 1
 
 print("Good Morning!")
-print("These keyboard commands are for the MagicArt 5 program.")
-print("F1 = .925 Template")
-print("F2 = 10K Template")
-print("F3 = 14K Template")
-print("F4 = Logos Template")
-print("F11 = Open SKU Search")
-print("F12 = Open MagicArt")
-print("Shift + Escape : Close Hotkeys Program")
+print("These keyboard commands are for the MagicArt 5 program:")
+print(" F1 = .925 Template")
+print(" F2 = 10K Template")
+print(" F3 = 14K Template")
+print(" F4 = Logos Template")
+print(" F11 = Open SKU Search")
+print(" F12 = Open MagicArt")
+print(" Shift + Escape : Close Hotkeys Program")
 print("Good luck, and have a nice day!")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
 
 
 
 def open_templates():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         
         print("Opening MagicArt")
         
-        #opening MagicArt program
         os.startfile("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\MagicArt 5\MagicART 5.lnk")
         time.sleep(usual_sleep_time + 1)
 
-        #making MagicArt fullscreen
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\fullscreen.png')
 
-        #minimizing object property box
         found = click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\object property pin.png')
         if not found:
             click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\highlighted open magicart.png')
@@ -57,7 +61,6 @@ def open_templates():
             click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\object property pin.png')
             click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\fullscreen.png')
 
-        #process to open templates
         for i in template_list:
             hotkey("ctrl", "o")
             time.sleep(usual_sleep_time)
@@ -66,19 +69,17 @@ def open_templates():
             hotkey("enter")
             time.sleep(usual_sleep_time)
             
-        #zoom for Logos Template
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\Logos Template.png')
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\fit to page.png')
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\10%.png')
         
-        #zoom for K Templates
         for k in zoom_list:
             print(f"Looking for {k}...")
             click_if_exists(k)
             click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\fit to page.png')
             click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\15%.png')
             
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 template_list = [
@@ -95,66 +96,84 @@ zoom_list = [
 
 
 def open_silver():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("Silver Command Triggered...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\Silver Template.png')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def open_10K():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("10K Command Triggered...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\10K Template.png')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def open_14K():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("14K Command Triggered...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\14K Template.png') 
-        dont_fuck_up = False
+        dfu = False
+    print("Command Successful")
+
+def open_google():
+    global dfu
+    if not dfu:
+        dfu = True
+        print("Opening Google")
+        webbrowser.open(f"https://www.google.com/")
+        dfu = False
+    print("Command Successful")
+
+def open_workday():
+    global dfu
+    if not dfu:
+        dfu = True
+        print("Opening Workday...")
+        webbrowser.open(f"https://www.myworkday.com/wday/authgwy/signetjewelers/login.htmld")
+        dfu = False
     print("Command Successful")
 
 def open_SKU_search():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("opening SKU finder...")
         os.startfile(r'C:\Users\rcherveny\Desktop\Keyboard Commands\sku_search.py')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def open_logos():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("Logos Command Triggered...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\Logos Template.png')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def center_allignment():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu= True
         print("Centering Object...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\center button.png')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def horizontal_allignment():
-    global dont_fuck_up
-    if not dont_fuck_up:
-        dont_fuck_up = True
+    global dfu
+    if not dfu:
+        dfu = True
         print("Alligning Horizontally...")
         click_if_exists(r'C:\Users\rcherveny\Desktop\Keyboard Commands\images\horizontal allign.png')
-        dont_fuck_up = False
+        dfu = False
     print("Command Successful")
 
 def exit_application():
@@ -169,6 +188,8 @@ bindings = [
     [["F3"], None, open_10K],
     [["F4"], None, open_14K],
     [["F5"], None, open_logos],
+    [["F9"], None, open_google],
+    [["F10"], None, open_workday],
     [["F11"], None, open_SKU_search],
     [["F12"], None, open_templates],
     [["control", "shift"], None, horizontal_allignment],
